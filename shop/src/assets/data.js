42e3cc305ae0cf9cbe151d2e83e704a8d77b8300
WENDY <97227279+wendy1126@users.noreply.github.com>
2023-01-04 12:05:22 +0900

    data.js 수정

--- shop/src/assets/data.js
+++ shop/src/assets/data.js
@@ -1,98 +1,98 @@
 export default [
   {
     id: 0,
-    title: "루카",
+    title: "주토피아",
     image:
       "https://raw.githubusercontent.com/wendy1126/Shop-image/main/img0.jpg",
-    content: "18년 신축공사한 남향 원룸 ☀️, 공기청정기 제공",
-    price: 340000,
+    content: "애니메이션, 액션, 모험, 코미디, 가족",
+    price: 12000,
   },
   {
     id: 1,
-    title: "Changdong Aurora Bedroom(Queen-size)",
+    title: "루카",
     image:
       "https://raw.githubusercontent.com/wendy1126/Shop-image/main/img1.jpg",
-    content: "침실만 따로 있는 공용 셰어하우스입니다. 최대 2인 가능",
-    price: 450000,
+    content: "애니메이션, 모험, 코미디",
+    price: 11500,
   },
   {
     id: 2,
-    title: "Geumsan Apartment Flat",
+    title: "코코",
     image:
       "https://raw.githubusercontent.com/wendy1126/Shop-image/main/img2.jpg",
-    content: "금산오거리 역세권 아파트입니다. 애완동물 불가능 ?",
-    price: 780000,
+    content: "애니메이션, 모험, 코미디",
+    price: 12500,
   },
   {
     id: 3,
-    title: "Double styled beds Studio Apt",
+    title: "굿다이노",
     image:
       "https://raw.githubusercontent.com/wendy1126/Shop-image/main/img3.jpg",
-    content: "무암동인근 2인용 원룸입니다. 전세 전환가능",
-    price: 550000,
+    content: "애니메이션, 모험, 코미디, 가족, 판타지",
+    price: 8900,
   },
   {
     id: 4,
-    title: "MyeongIl Apartment flat",
+    title: "온워드: 단 하루의 기적",
     image:
       "https://raw.githubusercontent.com/wendy1126/Shop-image/main/img4.jpg",
-    content: "탄천동 아파트 월세, 남향, 역 5분거리, 허위매물아님",
-    price: 680000,
+    content: "판타지, 모험",
+    price: 9500,
   },
   {
     id: 5,
-    title: "Banziha One Room",
+    title: "겨울왕국",
     image:
       "https://raw.githubusercontent.com/wendy1126/Shop-image/main/img5.jpg",
-    content: "반지하 원룸입니다. 비올 때 물가끔 새는거 빼면 좋아요",
-    price: 370000,
+    content: "애니메이션, 모험, 코미디, 가족, 판타지, 뮤지컬",
+    price: 25000,
   },
   {
     id: 6,
-    title: "Sinrim station 30 meters away",
+    title: "업",
     image:
       "https://raw.githubusercontent.com/wendy1126/Shop-image/main/img6.jpg",
-    content: "18년 신축공사한 남향 원룸 ☀️, 공기청정기 제공",
-    price: 340000,
+    content: "가족, 모험, 코미디, 액션, 애니메이션",
+    price: 18900,
   },
   {
     id: 7,
-    title: "Changdong Aurora Bedroom(Queen-size)",
+    title: "라푼젤",
     image:
       "https://raw.githubusercontent.com/wendy1126/Shop-image/main/img7.jpg",
-    content: "침실만 따로 있는 공용 셰어하우스입니다. 최대 2인 가능",
-    price: 450000,
+    content: "애니메이션, 코미디, 가족, 판타지, 뮤지컬, 멜로/로맨스",
+    price: 13000,
   },
   {
     id: 8,
-    title: "Geumsan Apartment Flat",
+    title: "토이스토리",
     image:
       "https://raw.githubusercontent.com/wendy1126/Shop-image/main/img8.jpg",
-    content: "금산오거리 역세권 아파트입니다. 애완동물 불가능 ?",
-    price: 780000,
+    content: "애니메이션, 모험, 코미디, 가족, 판타지",
+    price: 23500,
   },
   {
     id: 9,
-    title: "Double styled beds Studio Apt",
+    title: "레드슈즈",
     image:
       "https://raw.githubusercontent.com/wendy1126/Shop-image/main/img9.jpg",
-    content: "무암동인근 2인용 원룸입니다. 전세 전환가능",
-    price: 550000,
+    content: "가족, 애니메이션",
+    price: 12000,
   },
   {
     id: 10,
-    title: "MyeongIl Apartment flat",
+    title: "라이온 킹",
     image:
       "https://raw.githubusercontent.com/wendy1126/Shop-image/main/img10.jpg",
-    content: "탄천동 아파트 월세, 남향, 역 5분거리, 허위매물아님",
-    price: 680000,
+    content: "모험, 드라마, 가족",
+    price: 16500,
   },
   {
     id: 11,
-    title: "Banziha One Room",
+    title: "인사이드 아웃",
     image:
       "https://raw.githubusercontent.com/wendy1126/Shop-image/main/img11.jpg",
-    content: "반지하 원룸입니다. 비올 때 물가끔 새는거 빼면 좋아요",
-    price: 370000,
+    content: "애니메이션, 코미디",
+    price: 14000,
   },
 ];
